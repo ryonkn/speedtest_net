04c2c5f36b2abad45426650265f481520e0ead27
Ryo Nakano <ryo@ryonkn.com>
2019-11-03 17:28:34 +0900

    Add SecureRandom gem

--- lib/speedtest_net/latency.rb
+++ lib/speedtest_net/latency.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'typhoeus'
+require 'securerandom'
 
 module SpeedtestNet
   class Latency
--- lib/speedtest_net/upload.rb
+++ lib/speedtest_net/upload.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'curb'
+require 'securerandom'
 
 module SpeedtestNet
   class Upload
