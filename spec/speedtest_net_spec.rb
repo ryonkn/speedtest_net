c7e12d15db43f2154be5c8d36b505c695cabba14
Ryo Nakano <ryo@ryonkn.com>
2019-11-15 20:17:29 +0900

    Add test for SpeedtestNet.list_server

--- spec/speedtest_net_spec.rb
+++ spec/speedtest_net_spec.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
-RSpec.describe SpeedtestNet do # rubocop:disable RSpec/EmptyExampleGroup
+RSpec.describe SpeedtestNet do
+  describe '.list_server' do
+    context 'when access was' do
+      it 'success' do
+        VCR.use_cassettes [{ name: 'config' }, { name: 'server' }] do
+          servers = described_class.list_server
+          expect(servers.count).to be(8919)
+        end
+      end
+    end
+  end
 end
