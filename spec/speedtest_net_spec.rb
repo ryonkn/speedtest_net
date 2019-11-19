4589ff97323b8e10a136be3408cfe33ddbe4d10d
Ryo Nakano <ryo@ryonkn.com>
2019-11-19 17:16:47 +0900

    Add test for SpeedtestNet.run

--- spec/speedtest_net_spec.rb
+++ spec/speedtest_net_spec.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe SpeedtestNet do
+  describe '.run' do
+    subject(:run) { described_class.run }
+
+    let(:config) { build(:config) }
+    let(:server) { build(:server) }
+
+    before do
+      allow(SpeedtestNet::Config).to receive(:fetch).and_return(config)
+      allow(SpeedtestNet::Server).to receive(:best_server).and_return(server)
+      allow(SpeedtestNet::Download).to receive(:measure).and_return(123_456.789)
+      allow(SpeedtestNet::Upload).to receive(:measure).and_return(987_654.31)
+    end
+
+    it 'was valid' do
+      expect(run).to be_instance_of(SpeedtestNet::Result)
+    end
+  end
+
   describe '.list_server' do
     context 'when access was' do
       it 'success' do
