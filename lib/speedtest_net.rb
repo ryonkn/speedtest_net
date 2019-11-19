eaaa8f240483e0f0af51f308f54942f29cda5b9c
Ryo Nakano <ryo@ryonkn.com>
2019-11-19 14:59:02 +0900

    Move SpeedtestNet.run method

--- lib/speedtest_net.rb
+++ lib/speedtest_net.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 require 'speedtest_net/version'
-require 'speedtest_net/run'
 require 'speedtest_net/server'
+require 'speedtest_net/download'
+require 'speedtest_net/upload'
+require 'speedtest_net/result'
 
 module SpeedtestNet
   def self.run
-    SpeedtestNet::Run.call
+    config = SpeedtestNet::Config.fetch
+    server = SpeedtestNet::Server.best_server
+    download = SpeedtestNet::Download.measure(server)
+    upload = SpeedtestNet::Upload.measure(server)
+
+    SpeedtestNet::Config.clear_cache
+    SpeedtestNet::Result.new(config.client, server, download, upload)
   end
 
   def self.list_server
