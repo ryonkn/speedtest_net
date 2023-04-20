e9fa5b47efbcabb276fd994af5d7d98f5fe70731
Ryo Nakano <ryo.z.nakano@gmail.com>
2023-04-20 13:11:12 +0900

    Rename variables to meaningful names by rubocop

--- spec/speedtest_net/geo_spec.rb
+++ spec/speedtest_net/geo_spec.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe SpeedtestNet::Geo do
-  let(:point1) { described_class.new(51.48, 0.01) }
-  let(:point2) { described_class.new(35.71, 139.81) }
+  let(:point) { described_class.new(51.48, 0.01) }
+  let(:other_point) { described_class.new(35.71, 139.81) }
 
   describe '#distance' do
     it 'was valid' do
-      expect(point1.distance(point2).round).to eq(9_558_510)
+      expect(point.distance(other_point).round).to eq(9_558_510)
     end
   end
 
   describe '#radian_lat' do
     it 'was valid' do
-      expect(point1.radian_lat).to eq(0.8984954989266808)
+      expect(point.radian_lat).to eq(0.8984954989266808)
     end
   end
 
   describe '#radian_long' do
     it 'was valid' do
-      expect(point1.radian_long).to eq(0.00017453292519943296)
+      expect(point.radian_long).to eq(0.00017453292519943296)
     end
   end
 end
